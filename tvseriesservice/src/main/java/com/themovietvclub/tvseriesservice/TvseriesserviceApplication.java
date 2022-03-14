d9bcacc8558469974fcc76f1599cc4164fdaeb8e
Psayanth <psayanth.p@gmail.com>
2022-03-14 11:36:09 +0530

    Adding four services

---
+++ tvseriesservice/src/main/java/com/themovietvclub/tvseriesservice/TvseriesserviceApplication.java
@@ -0,0 +1,13 @@
+package com.themovietvclub.tvseriesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvseriesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvseriesserviceApplication.class, args);
+	}
+
+}
---
+++ tvseriesservice/src/test/java/com/themovietvclub/tvseriesservice/TvseriesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themovietvclub.tvseriesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvseriesserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
